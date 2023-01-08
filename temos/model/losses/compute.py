b8514a9123ce7c7c394f5333e04e5863b9853783
clemgris <clemence.grislain@eleves.enpc.fr>
2023-01-08 16:48:21 +0100

    condition on lenght

--- temos/model/losses/compute.py
+++ temos/model/losses/compute.py
@@ -82,10 +82,14 @@ class TemosComputeLosses(Module):
           velocities_i = torch.norm(((feats_i[2:]-feats_i[:-2])/2), dim=-1)[idx-4]
         #   print('velocities_i.shape', velocities_i.shape)
           
-          print(contacts_motion.shape, i, len(idx), min(idx), max(idx))
-
-          contact_motions_i = contacts_motion[i][idx]
-          contact_text_i = contacts_text[i][idx]
+          if max(idx) < contacts_motion.shape[1]:
+            contact_motions_i = contacts_motion[i][idx]
+            contact_text_i = contacts_text[i][idx]
+          else:
+            contact_motions_i = torch.zeros(1)
+            contact_text_i = torch.zeros(1)
+            print("Skipping shape mismatch in idx", max(idx), contacts_motion.shape[1])
+            
           contacts_ref_i = torch.Tensor(contacts_ref[i]).to(device)
           # velocities_ref_i = torch.Tensor(velocities_ref[i]).to(device)
         #   print('contacts_shape', contact_motions_i.shape, contact_text_i.shape, contacts_ref_i.shape)
